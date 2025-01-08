f88cdf89e0598f263ec42276fa043372380bd190
sabi <sabi@sabi.pink>
2025-01-07 21:58:07 -0600

    add on body exited for ghost

--- scripts/ghost.gd
+++ scripts/ghost.gd
@@ -1,5 +1,18 @@
 extends Area2D
 
 
+var camera_hovering: bool = false
+
+
 func _on_body_entered(_body:Node2D) -> void:
-  queue_free()
+  camera_hovering = true
+  print(camera_hovering)
+
+
+func _on_ready() -> void:
+  print(camera_hovering)
+
+
+func _on_body_exited(_body:Node2D) -> void:
+  camera_hovering = false
+  print(camera_hovering)

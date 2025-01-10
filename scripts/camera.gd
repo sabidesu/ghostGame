2492d4dbcf0109a2533c943e79972d3cf5b2038b
sabi <sabi@sabi.pink>
2025-01-10 00:55:30 -0600

    add reload timer to camera

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -1,14 +1,26 @@
 extends CharacterBody2D
 
 
+@onready var reload_timer = $ReloadTimer
+
 signal camera_shutter
+
+enum States {READY, DISABLED}
+
+var state: States = States.READY
+
+
+func _on_ready() -> void:
+	reload_timer.timeout.connect(_on_reload_timer_timeout)
 
 
 func _process(_delta: float) -> void:
 	camera_movement()
 
-	if Input.is_action_just_pressed("click"):
+	if state == States.READY && Input.is_action_just_pressed("click"):
 		camera_shutter.emit()
+		state = States.DISABLED
+		reload_timer.start()
 
 
 func camera_movement() -> void:
@@ -21,3 +33,7 @@ func camera_movement() -> void:
 		position.x = mouse_position_x
 	if mouse_position_y >= 0 && mouse_position_y <= screen_size_y:
 		position.y = mouse_position_y
+
+
+func _on_reload_timer_timeout() -> void:
+	state = States.READY
